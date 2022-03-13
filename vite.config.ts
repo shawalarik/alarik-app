df3cf62ac55256d8d38eab8dcc6ad9efe1667300
Shaw Alarik <alarikshaw@gmail.com>
2022-03-13 23:28:53 +0800

    init

---
+++ vite.config.ts
@@ -0,0 +1,22 @@
+import * as path from 'path'
+
+import Pages from 'vite-plugin-pages'
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: 
+  [
+    vue({
+      include: [/\.vue$/, /\.md$/],
+    }),
+    // https://github.com/hannoeru/vite-plugin-pages
+    Pages({
+      extensions: ['vue', 'md'],
+    }),
+  ],
+  alias: {
+    '~/': `${path.resolve(__dirname, 'src')}/`,
+  },
+})
